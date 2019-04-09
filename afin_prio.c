b9afa706428499dc791de41003b805d8e07bd25d
Michel <michelduarte2191@gmail.com>
2019-04-09 15:44:14 -0400

    final afin_prio

--- afin_prio.c
+++ afin_prio.c
@@ -5,6 +5,10 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
+#include <errno.h>
+
+#define handle_error_en(en, msg) \
+               do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
 
 #define NUM_THREADS 5
 #define THREAD_EXEC_TIME 2
@@ -13,254 +17,151 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void *threadFunction(void *threadId)
 {
-	long tid;
+	long tid, isItDiff;
 	tid = (long)threadId;
+	int s;
 
 	clockid_t cpuId;
 	struct timespec timeStart, timeStop;
 
-	long int isItDiff;
-
 	pthread_mutex_lock(&mutex);
 	pthread_mutex_unlock(&mutex);
 
-	pthread_getcpuclockid( pthread_self(), &cpuId );
-
-	clock_gettime( cpuId, &timeStart );
-	clock_gettime( cpuId, &timeStop );
+	s = pthread_getcpuclockid(pthread_self(), &cpuId);
+		if (s != 0)
+			handle_error_en(s, "thread: pthread_getcpuclockid");
+	s = clock_gettime(cpuId, &timeStart);
+		if (s != 0)
+			handle_error_en(s, "thread: clock_gettime");
+	s = clock_gettime(cpuId, &timeStop);
+		if (s != 0)
+			handle_error_en(s, "thread: clock_gettime");
 	
 	isItDiff = 0;
 	while( timeStop.tv_sec < timeStart.tv_sec + (long int)THREAD_EXEC_TIME )
 	{
-		clock_gettime( cpuId, &timeStop );
+		s = clock_gettime(cpuId, &timeStop);
+			if (s != 0)
+				handle_error_en(s, "thread: clock_gettime");
 		if( isItDiff != ( 2 - timeStop.tv_sec) )
 		{
 			isItDiff = ( 2 - timeStop.tv_sec );
-			printf("Thread %c here, and I need %ld more second(s).\n", (char)(tid+65), isItDiff);
+			if( isItDiff == 0 )
+				printf("Thread %c here, I am finished.\n", (char)(tid+65));
+			else
+				printf("Thread %c here, and I need %ld more second(s).\n", (char)(tid+65), isItDiff);
 		}
 
 	}
-
-	printf("Thread %c here, I am finished.\n", (char)(tid+65) );
 
 	pthread_exit(NULL);
 }
 
-int main()
+void createThreadFunc(int affin, int prio, int policy)
 {
-
 	pthread_t threads[NUM_THREADS];
 	struct sched_param param;
-	int i, rc, policy, prio_min_fifo, prio_max_fifo, prio_min_rr, prio_max_rr;
-	long tid;
-
-	prio_min_fifo = sched_get_priority_min(SCHED_FIFO);
-	prio_max_fifo = sched_get_priority_max(SCHED_FIFO);
-	
-	prio_min_rr = sched_get_priority_min(SCHED_RR);
-	prio_max_rr = sched_get_priority_max(SCHED_RR);
-
-	param.sched_priority = prio_max_fifo;
-	pthread_setschedparam( pthread_self(), SCHED_FIFO, &param );
-	printf("This parent process has %d priority.\n", param.sched_priority);
-
 	cpu_set_t cpuSet;
+	int i, s, prioMinFIFO, prioMaxFIFO, prioMinRR, prioMaxRR;
+	long tid;
+	
+	// Setup priorities
+	prioMinFIFO = sched_get_priority_min(SCHED_FIFO);
+	prioMaxFIFO = sched_get_priority_max(SCHED_FIFO);
 
-	pthread_getaffinity_np(pthread_self(), sizeof(cpuSet), &cpuSet);
-	for (i = 0; i < sysconf(_SC_NPROCESSORS_ONLN); i++)
+	prioMinRR = sched_get_priority_min(SCHED_RR);
+	prioMaxRR = sched_get_priority_max(SCHED_RR);
+
+	pthread_mutex_lock(&mutex);
+	for(tid=0; tid<NUM_THREADS; tid++)
 	{
-		if ( CPU_ISSET(i, &cpuSet) )
-			printf("%d ", i);
+		// create
+		s = pthread_create(&threads[tid], NULL, threadFunction, (void *)tid);
+			if (s != 0)
+				handle_error_en(s, "func: pthread_create");
+		// scheduling and priority
+		if( policy == SCHED_FIFO )
+		{
+			if( prio )
+				param.sched_priority = (int)(prioMinFIFO + tid);
+			else
+				param.sched_priority = (int)prioMaxFIFO/2;
+		}
+		else
+		{
+			if( prio )
+				param.sched_priority = (int)(prioMinRR + tid);
+			else
+				param.sched_priority = (int)prioMaxRR/2;
+		}
+		s = pthread_setschedparam(threads[tid], policy, &param);
+			if (s != 0)
+				handle_error_en(s, "func: pthread_setschedparam");
+		// allocate
+		CPU_ZERO(&cpuSet);
+		if( affin )
+			CPU_SET(0, &cpuSet);
+		else
+			CPU_SET( (int)tid % sysconf(_SC_NPROCESSORS_ONLN), &cpuSet);
+		s = pthread_setaffinity_np(threads[tid], sizeof(cpuSet), &cpuSet);
+			if (s != 0)
+				handle_error_en(s, "func: pthread_setaffinity_np");
+		
+		// show info
+		printf("Created thread %c, Prio: %d, Affin: ", (char)(tid+65), param.sched_priority );
+		CPU_ZERO(&cpuSet);
+		s = pthread_getaffinity_np(threads[tid], sizeof(cpuSet), &cpuSet);
+			if (s != 0)
+				handle_error_en(s, "func: pthread_getaffinity_np");
+		for (i = 0; i < sysconf(_SC_NPROCESSORS_ONLN); i++)
+			if (CPU_ISSET(i, &cpuSet))
+				printf("%d ", i);
+		printf("\n");
 	}
-	printf(".\n");
+	pthread_mutex_unlock(&mutex);
 
-	printf("Available Processors: %ld\n", sysconf(_SC_NPROCESSORS_ONLN));
-	CPU_ZERO(&cpuSet);
-	CPU_SET(0, &cpuSet);
-	pthread_setaffinity_np( pthread_self(), sizeof(cpuSet), &cpuSet );
-	printf("Parent process is running in processor ");
-	CPU_ZERO(&cpuSet);
-	pthread_getaffinity_np(pthread_self(), sizeof(cpuSet), &cpuSet);
-	for (i = 0; i < sysconf(_SC_NPROCESSORS_ONLN); i++)
-	{
-		if ( CPU_ISSET(i, &cpuSet) )
-			printf("%d ", i);
-	}
-	printf(".\n");
+	for(tid=0; tid<NUM_THREADS; tid++)
+		pthread_join(threads[tid], NULL);
+}
 
+int main()
+{
+	int i, s, rc, policy, prio_min_fifo, prio_max_fifo, prio_min_rr, prio_max_rr;
+	long tid;
+	struct sched_param param;
+
+	// Give parent process priority over threads
+	param.sched_priority = sched_get_priority_max(SCHED_FIFO);
+	s = pthread_setschedparam(pthread_self(), SCHED_FIFO, &param);
+		if (s != 0)
+			handle_error_en(s, "main: pthread_setschedparam");
+	printf("Parent process has maximum FIFO priority (%d)\n", param.sched_priority);
+
+	// Show CPUs
+	printf("Available CPUs: %ld\n", sysconf(_SC_NPROCESSORS_ONLN));
+
+	// Show Policies Code Number
 	printf("FIFO = %d, RR = %d, OTHER = %d\n\n", SCHED_FIFO, SCHED_RR, SCHED_OTHER);
 
-	// First item on the exercise:
-	CPU_ZERO(&cpuSet);
-	CPU_SET(0, &cpuSet);
-	param.sched_priority = (int)prio_max_fifo/2;
-
-	pthread_mutex_lock(&mutex);
+	// Show results
 	printf("Same affinity, same priority, FIFO:\n");
-	for(tid=0; tid<NUM_THREADS; tid++)
-	{
-		rc = pthread_create( &threads[tid], NULL, threadFunction, (void *)tid );
-		pthread_setaffinity_np(threads[tid], sizeof(cpuSet), &cpuSet);
-		pthread_setschedparam(threads[tid], SCHED_FIFO, &param);
-		printf("Created thread %c, Prio: %d, Afin: ", (char)(tid+65), param.sched_priority );
-		CPU_ZERO(&cpuSet);
-		pthread_getaffinity_np(threads[tid], sizeof(cpuSet), &cpuSet);
-		for (i = 0; i < sysconf(_SC_NPROCESSORS_ONLN); i++)
-		{
-			if ( CPU_ISSET(i, &cpuSet) )
-				printf("%d ", i);
-		}
-		printf("\n");
-	}
-	pthread_mutex_unlock(&mutex);
-	
-	for(tid=0; tid<NUM_THREADS; tid++)
-		pthread_join(threads[tid], NULL);
-	
+	createThreadFunc(1, 0, SCHED_FIFO);
 	printf("\n");
-
-	// Second item on the exercise:
-	CPU_ZERO(&cpuSet);
-	CPU_SET(0, &cpuSet);
-	param.sched_priority = (int)prio_max_rr/2;
-
-	pthread_mutex_lock(&mutex);
 	printf("Same affinity, same priority, RR:\n");
-	for(tid=0; tid<NUM_THREADS; tid++)
-	{
-		rc = pthread_create( &threads[tid], NULL, threadFunction, (void *)tid );
-		pthread_setaffinity_np(threads[tid], sizeof(cpuSet), &cpuSet);
-		pthread_setschedparam(threads[tid], SCHED_RR, &param);
-		printf("Created thread %c, Prio: %d, Afin: ", (char)(tid+65), param.sched_priority );
-		CPU_ZERO(&cpuSet);
-		pthread_getaffinity_np(threads[tid], sizeof(cpuSet), &cpuSet);
-		for (i = 0; i < sysconf(_SC_NPROCESSORS_ONLN); i++)
-		{
-			if ( CPU_ISSET(i, &cpuSet) )
-				printf("%d ", i);
-		}
-		printf("\n");
-	}
-	pthread_mutex_unlock(&mutex);
-	
-	for(tid=0; tid<NUM_THREADS; tid++)
-		pthread_join(threads[tid], NULL);
-
+	createThreadFunc(1, 0, SCHED_RR);
 	printf("\n");
-
-	// Third item on the exercise:
-	CPU_ZERO(&cpuSet);
-	CPU_SET(0, &cpuSet);
-
-	pthread_mutex_lock(&mutex);
 	printf("Same affinity, different priority, FIFO:\n");
-	for(tid=0; tid<NUM_THREADS; tid++)
-	{
-		rc = pthread_create( &threads[tid], NULL, threadFunction, (void *)tid );
-		pthread_setaffinity_np(threads[tid], sizeof(cpuSet), &cpuSet);
-		param.sched_priority = (int)(prio_min_fifo + prio_max_fifo*tid/10);
-		pthread_setschedparam(threads[tid], SCHED_FIFO, &param);
-		printf("Created thread %c, Prio: %d, Afin: ", (char)(tid+65), param.sched_priority );
-		CPU_ZERO(&cpuSet);
-		pthread_getaffinity_np(threads[tid], sizeof(cpuSet), &cpuSet);
-		for (i = 0; i < sysconf(_SC_NPROCESSORS_ONLN); i++)
-		{
-			if ( CPU_ISSET(i, &cpuSet) )
-				printf("%d ", i);
-		}
-		printf("\n");
-	}
-	pthread_mutex_unlock(&mutex);
-	
-	for(tid=0; tid<NUM_THREADS; tid++)
-		pthread_join(threads[tid], NULL);
-
+	createThreadFunc(1, 1, SCHED_FIFO);
 	printf("\n");
-
-	// Fourth item on the exercise:
-	CPU_ZERO(&cpuSet);
-	CPU_SET(0, &cpuSet);
-
-	pthread_mutex_lock(&mutex);
 	printf("Same affinity, different priority, RR:\n");
-	for(tid=0; tid<NUM_THREADS; tid++)
-	{
-		rc = pthread_create( &threads[tid], NULL, threadFunction, (void *)tid );
-		pthread_setaffinity_np(threads[tid], sizeof(cpuSet), &cpuSet);
-		param.sched_priority = (int)(prio_min_rr + prio_max_rr*tid/10);
-		pthread_setschedparam(threads[tid], SCHED_RR, &param);
-		printf("Created thread %c, Prio: %d, Afin: ", (char)(tid+65), param.sched_priority );
-		CPU_ZERO(&cpuSet);
-		pthread_getaffinity_np(threads[tid], sizeof(cpuSet), &cpuSet);
-		for (i = 0; i < sysconf(_SC_NPROCESSORS_ONLN); i++)
-		{
-			if ( CPU_ISSET(i, &cpuSet) )
-				printf("%d ", i);
-		}
-		printf("\n");
-	}
-	pthread_mutex_unlock(&mutex);
-	
-	for(tid=0; tid<NUM_THREADS; tid++)
-		pthread_join(threads[tid], NULL);
-
+	createThreadFunc(1, 1, SCHED_RR);
 	printf("\n");
-
-	// Fifth item on the exercise:
-	param.sched_priority = (int)prio_max_fifo/2;
-
-	pthread_mutex_lock(&mutex);
 	printf("Different affinity, same priority, FIFO:\n");
-	for(tid=0; tid<NUM_THREADS; tid++)
-	{
-		CPU_ZERO(&cpuSet);
-		CPU_SET( (int)tid % sysconf(_SC_NPROCESSORS_ONLN), &cpuSet);
-		rc = pthread_create( &threads[tid], NULL, threadFunction, (void *)tid );
-		pthread_setaffinity_np(threads[tid], sizeof(cpuSet), &cpuSet);
-		pthread_setschedparam(threads[tid], SCHED_FIFO, &param);
-		printf("Created thread %c, Prio: %d, Afin: ", (char)(tid+65), param.sched_priority );
-		CPU_ZERO(&cpuSet);
-		pthread_getaffinity_np(threads[tid], sizeof(cpuSet), &cpuSet);
-		for (i = 0; i < sysconf(_SC_NPROCESSORS_ONLN); i++)
-		{
-			if ( CPU_ISSET(i, &cpuSet) )
-				printf("%d ", i);
-		}
-		printf("\n");
-	}
-	pthread_mutex_unlock(&mutex);
-	
-	for(tid=0; tid<NUM_THREADS; tid++)
-		pthread_join(threads[tid], NULL);
-
+	createThreadFunc(0, 0, SCHED_FIFO);
 	printf("\n");
-
-	// Sixth item on the exercise:
-	param.sched_priority = (int)prio_max_rr/2;
-
-	pthread_mutex_lock(&mutex);
 	printf("Different affinity, same priority, RR:\n");
-	for(tid=0; tid<NUM_THREADS; tid++)
-	{
-		CPU_ZERO(&cpuSet);
-		CPU_SET( (int)tid % sysconf(_SC_NPROCESSORS_ONLN), &cpuSet);
-		rc = pthread_create( &threads[tid], NULL, threadFunction, (void *)tid );
-		pthread_setaffinity_np(threads[tid], sizeof(cpuSet), &cpuSet);
-		pthread_setschedparam(threads[tid], SCHED_RR, &param);
-		printf("Created thread %c, Prio: %d, Afin: ", (char)(tid+65), param.sched_priority );
-		CPU_ZERO(&cpuSet);
-		pthread_getaffinity_np(threads[tid], sizeof(cpuSet), &cpuSet);
-		for (i = 0; i < sysconf(_SC_NPROCESSORS_ONLN); i++)
-		{
-			if ( CPU_ISSET(i, &cpuSet) )
-				printf("%d ", i);
-		}
-		printf("\n");
-	}
-	pthread_mutex_unlock(&mutex);
-	
-	for(tid=0; tid<NUM_THREADS; tid++)
-		pthread_join(threads[tid], NULL);
+	createThreadFunc(0, 0, SCHED_RR);
+	printf("\n");
 	
 	//getchar();
 	
